066f4a7163da277943c9489fd62fa8574047c9a0
Owen Rauckman <orauckman@fanthreesixty.com>
2020-06-02 00:24:30 -0500

    Hard Coding previewText for now

--- src/components/articlePreview/index.js
+++ src/components/articlePreview/index.js
@@ -21,12 +21,16 @@ export default ({ posts }) => (
                 ></div>
               </div>
               <h3 css={styles.previewTitle}>{article.node.title}</h3>
-              <p
+              {/* <div
                 css={styles.previewText}
                 dangerouslySetInnerHTML={{
                   __html: article.node.description.childMarkdownRemark.html,
                 }}
-              />
+              /> */}
+              <p css={styles.previewText}>
+                A small precursor to what you are about to read. This should
+                probably never span over three lines
+              </p>
             </Link>
           </li>
         );
